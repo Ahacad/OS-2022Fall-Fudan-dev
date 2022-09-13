a674f27e395f0fbea34049212ab09f5729891fc4
zzzly3 <1683994033@qq.com>
2022-09-13 21:27:40 +0800

    update alloc_test

--- src/test/allocator.c
+++ src/test/allocator.c
@@ -2,10 +2,11 @@
 #include <kernel/printk.h>
 #include <aarch64/intrinsic.h>
 #include <common/rc.h>
+#include <test/test.h>
 
 extern RefCount alloc_page_cnt;
 
-static volatile int x = 0;
+RefCount x;
 static void* p[4][10000];
 
 void alloc_test()
@@ -15,28 +16,52 @@ void alloc_test()
     int y = 10000 - i * 500;
     if (i == 0)
         printk("alloc_page\n");
-    x++;
-    while (x < 4);
+    _increment_rc(&x);
+    while (x.count < 4);
     arch_dsb_sy();
     for (int j = 0; j < y; j++)
         p[i][j] = kalloc_page();
     for (int j = 0; j < y; j++)
         kfree_page(p[i][j]);
-    x++;
-    while (x < 8);
+    _increment_rc(&x);
+    while (x.count < 8);
     arch_dsb_sy();
     if (alloc_page_cnt.count != r)
         printk("FAIL: alloc_page_cnt %d -> %d\n", r, alloc_page_cnt);
     else
-        x++;
-    while (x < 12);
+        _increment_rc(&x);
+    while (x.count < 12);
     arch_dsb_sy();
-    for (int j = 0, t = i; j < y; j++)
-        p[i][j] = kalloc(((t = t + (t >> 6) + (t >> 12) + j) & 1023) + 1);
-    for (int j = 0; j < y; j++)
+    for (int j = 0; j < 10000; )
+    {
+        
+        if (j < 1000 || rand() > RAND_MAX / 3)
+        {
+            int z = (rand() & 1023) + 1;
+            p[i][j] = kalloc(z);
+            if (p[i][j] == NULL)
+            {
+                printk("FAIL: alloc(%d) = null\n", z);
+                while (1);
+            }
+            *(char*)p[i][j++] = i;
+        }
+        else
+        {
+            int k = rand() % j;
+            if (p[i][k] == NULL || *(char*)p[i][k] != i)
+            {
+                printk("FAIL: p[%d][%d] wrong\n", i, k);
+                while (1);
+            }
+            kfree(p[i][k]);
+            p[i][k] = p[i][--j];
+        }
+    }
+    for (int j = 0; j < 10000; j++)
         kfree(p[i][j]);
-    x++;
-    while (x < 16);
+    _increment_rc(&x);
+    while (x.count < 16);
     arch_dsb_sy();
     if (cpuid() == 0)
         printk("PASS\nUsage: %d\n", alloc_page_cnt.count - r);
