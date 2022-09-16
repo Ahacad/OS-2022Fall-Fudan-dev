e3712f11775f15fe98e828822964c9483bff7455
chenlida <ChenLD3124@163.com>
2022-09-16 13:49:40 +0800

    rbtree api

--- src/kernel/core.c
+++ src/kernel/core.c
@@ -9,6 +9,7 @@ bool panic_flag;
 NO_RETURN void idle_entry()
 {
     alloc_test();
+    rbtree_test();
     set_cpu_on();
     while (1)
     {
--- src/test/test.h
+++ src/test/test.h
@@ -3,5 +3,6 @@
 #define RAND_MAX 32768
 
 void alloc_test();
+void rbtree_test();
 unsigned rand();
 void srand(unsigned seed);
